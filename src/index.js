b0a1058397cf68dabb115543821cc9ec14149a4a
jeyden94 <jeyden94@gmail.com>
2025-08-26 17:04:33 -0500

    Added additional features from the Odin Project's second lesson on webpack. Mainly, broke out webpack.config.js into webpack.dev.js and webpack.prod.js.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 import odinImage from "./images/odin.png";
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 console.log(greeting);
-  
+
 const image = document.createElement("img");
 image.src = odinImage;
    
