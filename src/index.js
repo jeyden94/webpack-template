f84bc94b2f5f9b83914ce436367e13458780d156
jeyden94 <jeyden94@gmail.com>
2025-08-25 19:05:59 -0500

    Added all files from The Odin Project's first lesson pertaining to webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { greeting } from "./greeting.js";
+import odinImage from "./images/odin.png";
+
+console.log(greeting);
+  
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+
+ 
